ebf667bbd21be43bbaa6f78d951b4551d7e6fd6c
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-06-05 11:39:01 -0700

    Update samples to power of 2

--- run_scripts/run_5.sh
+++ run_scripts/run_5.sh
@@ -3,4 +3,4 @@
 mkdir -p experiments/exp_5/
 
 echo "EXPERIMENT 5 POPULATION $1" >> experiments/exp_5/exp-5.out
-nohup python sa.py -i sa_problems/exp-5_sa-problem.json -o ./exp_5 -m ./garisom/02_program_code -w 32 -s 35000 -p $1 2>&1 >> experiments/exp_5/exp-5.out &
+nohup python sa.py -i sa_problems/exp-5_sa-problem.json -o ./exp_5 -m ./garisom/02_program_code -w 32 -s 32768 -p $1 2>&1 >> experiments/exp_5/exp-5.out &
