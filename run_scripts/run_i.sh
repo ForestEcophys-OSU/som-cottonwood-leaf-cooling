2374d65c4a6eb00058ca8275c302d2863a71dffd
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-06-05 11:10:39 -0700

    Add an "i-th" experiment script

---
+++ run_scripts/run_i.sh
@@ -0,0 +1,7 @@
+# Generic SA script to run an i-th experiment
+
+EXP_NUM=$1
+POP=$2
+
+echo "EXPERIMENT ${EXP_NUM} POPULATION $POP" >> exp-${EXP_NUM}.out
+nohup python sa.py -i sa_problems/exp-${EXP_NUM}_sa-problem.json -o ./exp_${EXP_NUM} -m ./garisom/02_program_code -w 60 -s 35000 -p $POP 2>&1 >> exp_${EXP_NUM}/exp-${EXP_NUM}.out &
