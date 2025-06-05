24d3853b9be6271d75ac2378bac49dd8e9a6145a
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-06-05 11:25:35 -0700

    Update experiment 5 script

--- run_scripts/run_5.sh
+++ run_scripts/run_5.sh
@@ -1,4 +1,6 @@
 # SA of baperga, height, leafAreaIndex, and RootBeta
 
+mkdir experiments/exp_5/
+
 echo "EXPERIMENT 5 POPULATION $1" >> exp_5/exp-5.out
-nohup python sa.py -i sa_problems/exp-5_sa-problem.json -o ./exp_5 -m ./garisom/02_program_code -w 60 -s 35000 -p $1 2>&1 >> exp_5/exp-5.out &
+nohup python sa.py -i sa_problems/exp-5_sa-problem.json -o ./exp_5 -m ./garisom/02_program_code -w 60 -s 35000 -p $1 2>&1 >> experiments/exp_5/exp-5.out &
