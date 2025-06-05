2cc87339e2e60f038e26cff4effafaeed133b5b1
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-06-05 11:16:19 -0700

    Update to reflect environment and subfolder experiment output saving

--- run_scripts/run_5.sh
+++ run_scripts/run_5.sh
@@ -1,4 +1,4 @@
 # SA of baperga, height, leafAreaIndex, and RootBeta
 
-echo "EXPERIMENT 5 POPULATION $1" >> exp-5.out
+echo "EXPERIMENT 5 POPULATION $1" >> exp_5/exp-5.out
 nohup python sa.py -i sa_problems/exp-5_sa-problem.json -o ./exp_5 -m ./garisom/02_program_code -w 60 -s 35000 -p $1 2>&1 >> exp_5/exp-5.out &
--- run_scripts/run_i.sh
+++ run_scripts/run_i.sh
@@ -3,5 +3,5 @@
 EXP_NUM=$1
 POP=$2
 
-echo "EXPERIMENT ${EXP_NUM} POPULATION $POP" >> exp-${EXP_NUM}.out
+echo "EXPERIMENT ${EXP_NUM} POPULATION $POP" >> exp_${EXP_NUM}/exp-${EXP_NUM}.out
 nohup python sa.py -i sa_problems/exp-${EXP_NUM}_sa-problem.json -o ./exp_${EXP_NUM} -m ./garisom/02_program_code -w 60 -s 35000 -p $POP 2>&1 >> exp_${EXP_NUM}/exp-${EXP_NUM}.out &
