46b2b20680f86fa32f1f20d47aae1579d9aee67c
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-06-05 13:09:09 -0700

    Update reserve script for 7 days

--- reserve_garisom.sh
+++ reserve_garisom.sh
@@ -3,7 +3,7 @@
 #SBATCH -J garisom
 #SBATCH -c 32
 #SBATCH --mem=128G
-#SBATCH --time=1-00:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --mail-type=ALL,TIMELIMIT_90 # send email notification when job reaches 90% of time limit
 #SBATCH --mail-user=pannikkc@oregonstate.edu
 
