929a57632e6169e98a112f340246a7c8cd67709d
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-06-10 21:00:10 -0700

    Update run_i script

--- run_scripts/run_i.sh
+++ run_scripts/run_i.sh
@@ -2,6 +2,9 @@
 
 EXP_NUM=$1
 POP=$2
+N=$3
 
-echo "EXPERIMENT ${EXP_NUM} POPULATION $POP" >> exp_${EXP_NUM}/exp-${EXP_NUM}.out
-nohup python sa.py -i sa_problems/exp-${EXP_NUM}_sa-problem.json -o ./exp_${EXP_NUM} -m ./garisom/02_program_code -w 60 -s 35000 -p $POP 2>&1 >> exp_${EXP_NUM}/exp-${EXP_NUM}.out &
+mkdir -p experiments/exp_${EXP_NUM}/
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - EXPERIMENT ${EXP_NUM} POPULATION $POP" >> experiments/exp_${EXP_NUM}/exp-${EXP_NUM}.out
+nohup python sa.py -i sa_problems/exp-${EXP_NUM}_sa-problem.json -o ./experiments/exp_${EXP_NUM} -m ./garisom/02_program_code -w 30 -s $N -p $POP 2>&1 >> experiments/exp_${EXP_NUM}/exp-${EXP_NUM}.out &
