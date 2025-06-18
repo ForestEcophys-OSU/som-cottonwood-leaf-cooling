a106d9fb791f4a747d510434626a97b025b80812
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-06-18 14:31:45 -0700

    Reorganize run-script file structure by population

--- run_scripts/ccr/run_i.sh
+++ run_scripts/ccr/run_i.sh
@@ -8,4 +8,4 @@ W=$4
 mkdir -p experiments/exp_${EXP_NUM}/
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - EXPERIMENT ${EXP_NUM} POPULATION $POP" >> experiments/exp_${EXP_NUM}/exp-${EXP_NUM}.out
-nohup python sa.py -i sa_problems/exp-${EXP_NUM}_sa-problem.json -o ./experiments/exp_${EXP_NUM} -m ./garisom/02_program_code -w $W -s $N -p $POP 2>&1 >> experiments/exp_${EXP_NUM}/exp-${EXP_NUM}.out &
+nohup python sa.py -i sa_problems/ccr/exp-${EXP_NUM}_sa-problem.json -o ./experiments/exp_${EXP_NUM} -m ./garisom/02_program_code -w $W -s $N -p $POP 2>&1 >> experiments/exp_${EXP_NUM}/exp-${EXP_NUM}.out &
