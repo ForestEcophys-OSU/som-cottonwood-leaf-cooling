fade0ef2ea3425043df87fd7a22099276c2c7352
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-06-24 11:32:21 -0700

    Update test script to ccr path

--- run_scripts/test.sh
+++ run_scripts/test.sh
@@ -3,4 +3,4 @@
 mkdir -p experiments/test/
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - TEST RUN POPULATION $1" >> experiments/test/test.out
-nohup python sa.py -i sa_problems/exp-5_sa-problem.json -o ./experiments/test -m ./garisom/02_program_code -w 32 -s 2 -p $1 2>&1 >> experiments/test/test.out &
+nohup python sa.py -i sa_problems/ccr/exp-5_sa-problem.json -o ./experiments/test -m ./garisom/02_program_code -w 32 -s 2 -p $1 2>&1 >> experiments/test/test.out &
