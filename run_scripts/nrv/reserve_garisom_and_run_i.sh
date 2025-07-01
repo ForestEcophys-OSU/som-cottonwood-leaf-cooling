a683402e41a4b1431ff3814b3043953ae1a09b2c
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-07-01 15:23:00 -0700

    Update sbatch reserve scripts for NRV and TSZ to run in foreground

--- run_scripts/nrv/reserve_garisom_and_run_i.sh
+++ run_scripts/nrv/reserve_garisom_and_run_i.sh
@@ -10,4 +10,14 @@
 cd ~/hpc-share/fremont-cottonwood-dbg-garisom/
 source env/bin/activate
 
-./run_scripts/nrv/run_i.sh 1 4096 32
+EXP_NUM=1
+N=4096
+W=32
+
+EXP_DIR="experiments/nrv/exp_${EXP_NUM}"
+EXP_OUT="$EXP_DIR/exp-${EXP_NUM}.out"
+
+mkdir -p $EXP_DIR
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - EXPERIMENT ${EXP_NUM} POPULATION $POP" >> $EXP_OUT
+python sa.py -i sa_problems/nrv/exp-${EXP_NUM}_sa-problem.json -o $EXP_DIR -m ./garisom/02_program_code -w $W -s $N -p 3 2>&1 >> $EXP_OUT
