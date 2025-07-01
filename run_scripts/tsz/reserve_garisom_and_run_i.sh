02556ae60c4201d5be9bf82cdc7ef1b8f61fc4ad
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-07-01 15:24:35 -0700

    Update TSZ sbatch script

--- run_scripts/tsz/reserve_garisom_and_run_i.sh
+++ run_scripts/tsz/reserve_garisom_and_run_i.sh
@@ -20,4 +20,4 @@ EXP_OUT="$EXP_DIR/exp-${EXP_NUM}.out"
 mkdir -p $EXP_DIR
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - EXPERIMENT ${EXP_NUM} POPULATION $POP" >> $EXP_OUT
-nohup python sa.py -i sa_problems/tsz/exp-${EXP_NUM}_sa-problem.json -o $EXP_DIR -m ./garisom/02_program_code -w $W -s $N -p 4 2>&1 >> $EXP_OUT &
+python sa.py -i sa_problems/tsz/exp-${EXP_NUM}_sa-problem.json -o $EXP_DIR -m ./garisom/02_program_code -w $W -s $N -p 4 2>&1 >> $EXP_OUT
