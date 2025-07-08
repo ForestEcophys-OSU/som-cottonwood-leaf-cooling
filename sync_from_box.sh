f57bf158ba10bb46827a349e7e8f3e955718c9b6
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-07-08 11:05:50 -0700

    Add updated box sync scripts.

--- sync_from_box.sh
+++ sync_from_box.sh
@@ -1,2 +1,3 @@
-rclone copy --exclude-from='.gitignore' box:dbg-out/ ./experiments -v
+rclone copy --exclude-from='.gitignore' box:dbg-out/experiments ./sensitivity-analysis/experiments -v
+rclone copy --exclude-from='.gitignore' box:dbg-out/optimization ./optimization/output -v
 # rclone check --exclude-from='.gitignore' ./experiments box:dbg-out/
--- sync_to_box.sh
+++ sync_to_box.sh
@@ -1,2 +1,3 @@
-rclone copy --exclude-from='.gitignore' ./experiments box:dbg-out/ -v
+rclone copy --exclude-from='.gitignore' ./sensitivity-analysis/experiments box:dbg-out/experiments -v
+rclone copy --exclude-from='.gitignore' ./optimization/output box:dbg-out/optimization -v
 # rclone check --exclude-from='.gitignore' ./experiments box:dbg-out/
