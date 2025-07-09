a56e747bff400cac52c9c40c31bc7e83e4248ad7
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-07-09 14:30:32 -0700

    Once again, update the sbatch scripts with the proper mkdir path.

--- optimization/run_configs/ccr/reserve_and_run_leaftemp.sh
+++ optimization/run_configs/ccr/reserve_and_run_leaftemp.sh
@@ -15,7 +15,7 @@ EXP="leaftemp"
 EXP_DIR="output/ccr"
 EXP_OUT="$EXP_DIR/${EXP}/optim.out"
 
-mkdir -p $EXP_DIR
+mkdir -p "${EXP_DIR}/${EXP}/"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - EXPERIMENT ${EXP} POPULATION CCR" >> $EXP_OUT
 python run_param_optim.py -i run_configs/ccr/${EXP}.json -o $EXP_DIR -m ../garisom/02_program_code 2>&1 >> $EXP_OUT
