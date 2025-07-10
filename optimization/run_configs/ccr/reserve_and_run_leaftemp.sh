02e4a35af8849d0352890407e9ddb6754425813a
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-07-10 13:52:41 -0700

    Ensure newest gcc version is loaded in slurm

--- optimization/run_configs/ccr/reserve_and_run_leaftemp.sh
+++ optimization/run_configs/ccr/reserve_and_run_leaftemp.sh
@@ -7,6 +7,8 @@
 #SBATCH --mail-type=ALL,TIMELIMIT_90 # send email notification when job reaches 90% of time limit
 #SBATCH --mail-user=pannikkc@oregonstate.edu
 
+ml rclone gcc/14.2
+
 cd ~/hpc-share/fremont-cottonwood-dbg-garisom/
 
 cd ./garisom/02_program_code
@@ -27,6 +29,5 @@ mkdir -p "${EXP_DIR}/${EXP}/"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - EXPERIMENT ${EXP} POPULATION CCR" >> $EXP_OUT
 python run_param_optim.py -i run_configs/ccr/${EXP}.json -o $EXP_DIR -m ../garisom/02_program_code 2>&1 >> $EXP_OUT
 
-ml rclone
 cd ..
 ./bash/sync_to_box.sh
