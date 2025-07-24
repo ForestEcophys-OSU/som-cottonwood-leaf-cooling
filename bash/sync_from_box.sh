990b0395b321d8bd66da379e957609543169d6e4
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-07-24 14:53:00 -0700

    Update box sync script to include montecarlo output

--- bash/sync_from_box.sh
+++ bash/sync_from_box.sh
@@ -1,3 +1,4 @@
 rclone copy --exclude-from='.gitignore' box:dbg-out/experiments ./sensitivity-analysis/experiments -v
 rclone copy --exclude-from='.gitignore' box:dbg-out/optimization ./optimization/output -v
+rclone copy --exclude-from='.gitignore' box:dbg-out/montecarlo ./montecarlo/output -v
 # rclone check --exclude-from='.gitignore' ./experiments box:dbg-out/
--- bash/sync_to_box.sh
+++ bash/sync_to_box.sh
@@ -1,3 +1,4 @@
 rclone copy --exclude-from='.gitignore' ./sensitivity-analysis/experiments box:dbg-out/experiments -v
 rclone copy --exclude-from='.gitignore' ./optimization/output box:dbg-out/optimization -v
+rclone copy --exclude-from='.gitignore' ./montecarlo/output box:dbg-out/montecarlo -v
 # rclone check --exclude-from='.gitignore' ./experiments box:dbg-out/
