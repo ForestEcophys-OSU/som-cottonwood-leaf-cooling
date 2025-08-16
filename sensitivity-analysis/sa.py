56c425eea22d285d542f571b8668821f142e2f9e
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-08-16 12:32:50 -0700

    Integrate sensitivity analysis script with garisom-tools library

--- sensitivity-analysis/sa.py
+++ sensitivity-analysis/sa.py
@@ -1,341 +1,34 @@
-import subprocess
-import os
-from tempfile import TemporaryDirectory
-from concurrent.futures import ThreadPoolExecutor, as_completed
 from argparse import ArgumentParser
-import json
-from collections import defaultdict
-from tqdm import tqdm
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-from SALib.sample import sobol as ssobol
-from SALib.analyze import sobol as asobol
-from sklearn.metrics import (
-    mean_squared_error,
-    r2_score,
-    root_mean_squared_error,
-    mean_absolute_percentage_error,
-    median_absolute_error
+from garisom_tools.sa import (
+    SensitivityAnalysisConfig,
+    SensitivityAnalysis
 )
+from garisom_tools import GarisomModel
+import pandas as pd
+import os
 from datetime import datetime
 
 
-def run_single_model(
-    X: np.ndarray,
-    in_names: list,
-    out_names: list,
-    proc_num: int,
-    tmp_dir: str,
-    params: pd.DataFrame,
-    POP_NUM: int,
-    CONFIG_FILE: str,
-    MODEL_DIR: str,
-    VERBOSE: bool,
-    RES_DIR: str
-) -> list[np.ndarray]:
+def get_ground_truth(population: int):
+    ground_dir = "../data/ground"
+    match population:
+        case 1:
+            ground = pd.read_csv(os.path.abspath(f"{ground_dir}/ccr_hourly_data.csv"))
+        case 2:
+            ground = pd.read_csv(os.path.abspath(f"{ground_dir}/jla_hourly_data.csv"))
+        case 3:
+            ground = pd.read_csv(os.path.abspath(f"{ground_dir}/nrv_hourly_data.csv"))
+        case 4:
+            ground = pd.read_csv(os.path.abspath(f"{ground_dir}/tsz_hourly_data.csv"))
+        case _:
+            raise Exception("Incorrect POP_NUM!")
 
-    # Get unique TMP_DIR and make directory for specific process
-    TMP_DIR = f"{tmp_dir}/{proc_num}"
-    TMP_PARAM_FILE = f"{TMP_DIR}/params.csv"
-    os.makedirs(TMP_DIR, exist_ok=True)
-
-    # Overwrite parameters with sample params
-    for i, name in enumerate(in_names):
-        params.at[POP_NUM - 1, name] = X[i]
-
-    # Setup parameter, configuration, and output files
-    params.to_csv(TMP_PARAM_FILE, index=False)
-
-    # Check if verbosity is enabled for saving model stdout
-    if VERBOSE:
-        os.makedirs(f"{RES_DIR}/stdout/", exist_ok=True)
-        out = open(f"{RES_DIR}/stdout/{proc_num}.txt", "w")
-    else:
-        out = subprocess.DEVNULL
-
-    CONFIG_FILE = os.path.abspath(CONFIG_FILE)
-
-    p = subprocess.run(
-        [
-            "./run",
-            TMP_PARAM_FILE,
-            CONFIG_FILE,
-            str(POP_NUM),
-            TMP_DIR
-        ],
-        cwd=MODEL_DIR,
-        stdout=out,
-        stderr=out
-    )
-
-    if VERBOSE:
-        out.close()
-
-    if p.returncode != 0:
-        raise RuntimeError(
-            f"Subprocess {proc_num} failed with exit code {p.returncode}"
-        )
-
-    # Get species, region, and site to determine output file
-    species = params.at[POP_NUM - 1, 'i_sp']
-    region = params.at[POP_NUM - 1, 'i_region']
-    site = params.at[POP_NUM - 1, 'i_site']
-
-    output_file = os.path.join(
-        TMP_DIR, f"timesteps_output_{species}_{region}_{site}.csv"
-    )
-    if not os.path.exists(output_file):
-        raise FileNotFoundError(
-            f"Expected output file not found: {output_file}"
-        )
-
-    output_file = pd.read_csv(output_file)
-
-    output = output_file[out_names].to_numpy(dtype=float)  # T x Y_D
-
-    return output
+    return ground
 
 
-def wrapped_garisom(
-    X: np.ndarray,
-    in_names: list,
-    out_names: list,
-    MAX_WORKERS: int,
-    MODEL_DIR: str,
-    params: pd.DataFrame,
-    POP_NUM: int,
-    CONFIG_FILE: str,
-    T: int,
-    VERBOSE: bool,
-    RES_DIR: str
-) -> np.ndarray:
-
-    N, D = X.shape
-    Y_D = len(out_names)
-    print(f"{N} samples")
-    res = np.empty((T, N, Y_D))
-
-    with TemporaryDirectory() as tmp:
-        print('created temporary directory', tmp)
-        pbar = tqdm(total=N)
-
-        with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-            futures = {
-                executor.submit(
-                    run_single_model,
-                    X[i, :],
-                    in_names,
-                    out_names,
-                    i,
-                    tmp,
-                    params,
-                    POP_NUM,
-                    CONFIG_FILE,
-                    MODEL_DIR,
-                    VERBOSE,
-                    RES_DIR
-                ): i for i in range(N)
-            }
-
-            for future in as_completed(futures):
-                pbar.update(1)
-                idx = futures[future]
-                try:
-                    out = future.result()
-                    res[:, idx, :] = out
-                except Exception as e:
-                    print(f"Subprocess for index {idx} failed: {e}")
-                    res[:, idx, :] = np.nan
-
-        pbar.close()
-
-    return res
-
-
-def calc_errors(
-    outputs,
-    ground,
-    problem
-):
-    start_day = problem['plot_settings']['start_day']
-    end_day = problem['plot_settings']['end_day']
-
-    def cmp_pred_to_ground_metrics(n_ground, n_pred):
-
-        fits = defaultdict(list)
-
-        for ground, pred in zip(n_ground, n_pred):
-
-            mse = mean_squared_error(ground, pred)
-            rmse = root_mean_squared_error(ground, pred)
-            mape = mean_absolute_percentage_error(ground, pred)
-            made = median_absolute_error(ground, pred)
-            r2 = r2_score(ground, pred)
-
-            fits['mse'].append(mse)
-            fits['rmse'].append(rmse)
-            fits['mape'].append(mape)
-            fits['made'].append(made)
-            fits['r2'].append(r2)
-
-        return fits
-
-    errors = {}
-    for idx, output_name in enumerate(problem['outputs']):
-
-        # Filter ground data based on julian-day and drop NaN values
-        col_ground = ground[
-            ground['julian-day'].between(start_day, end_day)
-        ][output_name].dropna()
-
-        # Align predictions with the filtered ground data
-        col_pred = outputs[:, :, idx]  # (N, T)
-        col_pred = pd.DataFrame(col_pred)
-        pred_values = col_pred.loc[col_ground.index].T.to_numpy()
-
-        # Get N copies of ground values
-        ground_values = np.array([col_ground.copy().to_numpy()
-                                  for _ in range(pred_values.shape[0])])
-
-        errors[output_name] = cmp_pred_to_ground_metrics(ground_values,
-                                                         pred_values)
-
-    return errors
-
-
-def plot(
-    first_order_indices,  # shape: (Y_D, D, T)
-    total_order_indices,  # shape: (Y_D, D, T)
-    outputs,              # shape: (T, N, Y_D)
-    errors,
-    param_values,
-    plt_dir,
-    T,
-    problem,
-):
-
-    # Plot settings
-    # average = problem['plot_settings']['average']
-    metric = problem['plot_settings']['metric']
-
-    time = np.arange(T)
-
-    # Plot First-Order Sobol Indices
-    for idx in range(0, len(problem['outputs']), 2):
-        fig, axes = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
-
-        for i, name in enumerate(problem['names']):
-            axes[0].scatter(time,
-                            first_order_indices[idx, i, :],
-                            label=f'{name}')
-            if idx + 1 < len(problem['outputs']):
-                axes[1].scatter(time,
-                                first_order_indices[idx + 1, i, :],
-                                label=f'{name}')
-
-        axes[0].set_title(
-            f'First-Order Sobol Indices for {problem["outputs"][idx]}'
-        )
-        if idx + 1 < len(problem['outputs']):
-            axes[1].set_title(
-                f'First-Order Sobol Indices for {problem["outputs"][idx + 1]}'
-            )
-
-        for ax in axes:
-            ax.set_ylabel('Sobol Index')
-            ax.legend()
-            ax.grid(True)
-
-        axes[1].set_xlabel('Time')
-        plt.tight_layout()
-
-        if idx + 1 < len(problem["outputs"]):
-            plt.savefig(
-                f"{plt_dir}/first-order_{problem['outputs'][idx]}_"
-                f"{problem['outputs'][idx + 1]}.png"
-            )
-        else:
-            plt.savefig(f"{plt_dir}/first-order_{problem['outputs'][idx]}.png")
-        plt.close(fig)
-
-    # Plot Total-Order Sobol Indices
-    for idx in range(0, len(problem['outputs']), 2):
-        fig, axes = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
-
-        for i, name in enumerate(problem['names']):
-            axes[0].scatter(time,
-                            total_order_indices[idx, i, :],
-                            label=f'{name}')
-            if idx + 1 < len(problem['outputs']):
-                axes[1].scatter(time,
-                                total_order_indices[idx + 1, i, :],
-                                label=f'{name}')
-
-        axes[0].set_title(
-            f'Total-Order Sobol Indices for {problem["outputs"][idx]}'
-        )
-        if idx + 1 < len(problem['outputs']):
-            axes[1].set_title(
-                f'Total-Order Sobol Indices for {problem["outputs"][idx + 1]}'
-            )
-
-        for ax in axes:
-            ax.set_ylabel('Sobol Index')
-            ax.legend()
-            ax.grid(True)
-
-        axes[1].set_xlabel('Time')
-        plt.tight_layout()
-
-        if idx + 1 < len(problem["outputs"]):
-            plt.savefig(
-                f"{plt_dir}/total-order_{problem['outputs'][idx]}_"
-                f"{problem['outputs'][idx + 1]}.png"
-            )
-        else:
-            plt.savefig(f"{plt_dir}/total-order_{problem['outputs'][idx]}.png")
-        plt.close(fig)
-
-    for idx, output_name in enumerate(problem['outputs']):
-        fig, axes = plt.subplots(len(problem['names']),
-                                 1,
-                                 figsize=(10, 8),
-                                 sharex=True)
-
-        for i, name in enumerate(problem['names']):
-            plt.figure(figsize=(10, 6))
-            plt.scatter(param_values[:, i],
-                        outputs[:, :, idx].mean(axis=0),
-                        label=f'{output_name}')
-            plt.title(f'Mean Output ({output_name}) vs {name}')
-            plt.xlabel(name)
-            plt.ylabel('Mean Output')
-            plt.legend()
-            plt.grid(True)
-
-            plt.tight_layout()
-            plt.savefig(f"{plt_dir}/mean_output_{output_name}_vs_{name}.png")
-            plt.close(fig)
-
-    # Plot and save the errors for each output in errors
-    for output_name, error_values in errors.items():
-        for i, param in enumerate(problem['names']):
-            plt.figure(figsize=(10, 6))
-            plt.scatter(param_values[:, i],
-                        error_values[metric],
-                        marker='o',
-                        label=f'{metric}')
-            plt.title(f'Error Metrics Across Samples for {output_name}')
-            plt.xlabel(f'{param}')
-            # plt.ylabel('Mean Absolute Percent Error (MAPE)')
-            plt.grid(True)
-            plt.legend()
-            plt.tight_layout()
-            plt.savefig(f"{plt_dir}/error_{metric}_{param}_{output_name}.png")
-            plt.close()
+def get_parameter_and_configuration_files() -> tuple[str, pd.DataFrame]:
+    return os.path.abspath("../DBG/configuration.csv"), \
+        pd.read_csv("../DBG/parameters.csv")
 
 
 def main():
@@ -359,24 +52,6 @@ def main():
         type=str
     )
     parser.add_argument(
-        "--workers", "-w",
-        help="Number of workers to run SA with.",
-        default=4,
-        type=int
-    )
-    parser.add_argument(
-        "--samples", "-s",
-        help="Number of samples to run SA with, must be a multiple of two.",
-        default=2**4,
-        type=int
-    )
-    parser.add_argument(
-        "--pop", "-p",
-        help="Population number to run for model.",
-        default=1,
-        type=int
-    )
-    parser.add_argument(
         "--verbose", "-v",
         help="Enable saving of model stdout.",
         action="store_true"
@@ -384,158 +59,50 @@ def main():
 
     args = parser.parse_args()
 
-    PARAM_FILE = "../DBG/parameters.csv"
-    CONFIG_FILE = "../DBG/configuration.csv"
-    DATA_FILE = "../DBG/dataset.csv"
-    POP_NUM = args.pop
-    VERBOSE = args.verbose
-    MAX_WORKERS = args.workers
-    SAMPLES = args.samples
-    MODEL_DIR = os.path.join(args.model)
-    OUT_DIR = os.path.join(args.output)
-    RAND_DIR = datetime.now().strftime('%Y%m%d_%H%M%S')
-    PLT_DIR = os.path.join(OUT_DIR, "plots", RAND_DIR)
-    RES_DIR = os.path.join(OUT_DIR, "sa_results", RAND_DIR)
+    model_dir = os.path.abspath(args.model)
+    rand_dir = datetime.now().strftime('%Y%m%d_%H%M%S')
 
-    print("Plots will be saved in: ", PLT_DIR)
-    print("Results will be saved in: ", RES_DIR)
+    out_dir = os.path.join(os.path.abspath(args.output), rand_dir)
+    os.makedirs(out_dir, exist_ok=True)
+    config_file = os.path.join(out_dir, "config.json")
 
-    os.makedirs(PLT_DIR, exist_ok=True)
-    os.makedirs(RES_DIR, exist_ok=True)
+    # Get configuration and save
+    config = SensitivityAnalysisConfig.from_json(args.input)
+    # Copy the input config file to the output config_file path
+    with open(args.input, "r") as src, open(config_file, "+x") as dst:
+        dst.write(src.read())
 
-    params = pd.read_csv(PARAM_FILE)
-    dataset = pd.read_csv(DATA_FILE)
+    # Get ground truth values
+    ground = get_ground_truth(config.pop)
 
-    T = dataset.shape[0]
+    # Get original parameter and configuration files
+    model_config, params = get_parameter_and_configuration_files()
 
-    # Loads 'D' parameters for SA
-    problem = None
-    with open(args.input, "+r") as f:
-        problem = json.load(f)
+    run_kwargs = {
+        'config_file': model_config,
+        'params': params,
+        'model_dir': model_dir,
+        'population': config.pop,
+        'verbose': args.verbose
+    }
 
-    # Save the problem definition to a JSON file in the results directory
-    problem_file = os.path.join(RES_DIR, "problem.json")
-    with open(problem_file, "w") as f:
-        problem['arguments'] = vars(args)  # add arguments to problem
-        json.dump(problem, f, indent=4)
+    eval_kwargs = {
+        'ground': ground,
+        'start_day': config.start_day,
+        'end_day': config.end_day,
+    }
 
-    D = problem['num_vars']
-    Y_D = len(problem['outputs'])
-
-    param_values = ssobol.sample(problem,
-                                 N=SAMPLES,
-                                 calc_second_order=False)  # shape : (N, D)
-
-    np.save(f"{RES_DIR}/sample.npy", param_values)
-
-    print("Running model with samples.")
-    Y = wrapped_garisom(    # returns shape: (T, N, Y_D)
-        param_values,
-        problem['names'],
-        problem['outputs'],
-        MAX_WORKERS,
-        MODEL_DIR,
-        params,
-        POP_NUM,
-        CONFIG_FILE,
-        T,
-        VERBOSE,
-        RES_DIR
+    model = GarisomModel(
+        run_kwargs=run_kwargs,
+        eval_kwargs=eval_kwargs
     )
 
-    Y = np.nan_to_num(Y)  # Convert all NaN values to 0
-
-    np.save(f"{RES_DIR}/model_output.npy", Y)
-
-    first_order_indices = np.empty((Y_D, D, T))
-    first_order_conf = np.empty((Y_D, D, T))
-    total_order_indices = np.empty((Y_D, D, T))
-    total_order_conf = np.empty((Y_D, D, T))
-    # second_order_indices = np.empty((Y_D, D, T))  # Not used
-    error_first_order_indices = np.empty((Y_D, D))
-    error_first_order_conf = np.empty((Y_D, D))
-    error_total_order_indices = np.empty((Y_D, D))
-    error_total_order_conf = np.empty((Y_D, D))
-    # second_order_indices = np.empty((Y_D, D))  # Not used
-
-    print("Analyzing output dimension indices.")
-    for t in range(T):
-        for i, out in enumerate(problem['outputs']):
-            si = asobol.analyze(problem, Y[t, :, i],
-                                print_to_console=False,
-                                calc_second_order=False,
-                                # parallel=True,
-                                # n_processors=MAX_WORKERS
-                                )
-            first_order_indices[i, :, t] = np.clip(si['S1'], 0, 1)
-            first_order_conf[i, :, t] = si['S1_conf']
-            total_order_indices[i, :, t] = np.clip(si['ST'], 0, 1)
-            total_order_conf[i, :, t] = si['ST_conf']
-            # second_order_indices[i, :, t] = np.clip(si['S2'], 0, 1)
-
-    np.save(f"{RES_DIR}/first_order_indices.npy", first_order_indices)
-    np.save(f"{RES_DIR}/first_order_conf.npy", first_order_conf)
-    np.save(f"{RES_DIR}/total_order_indices.npy", total_order_indices)
-    np.save(f"{RES_DIR}/total_order_conf.npy", total_order_conf)
-
-    ground = None
-    match POP_NUM:
-        case 1:
-            ground = pd.read_csv("../data/ccr_hourly_data.csv")
-        case 2:
-            ground = pd.read_csv("../data/jla_hourly_data.csv")
-        case 3:
-            ground = pd.read_csv("../data/tsz_hourly_data.csv")
-        case 4:
-            ground = pd.read_csv("../data/nrv_hourly_data.csv")
-        case _:
-            raise Exception("Incorrect POP_NUM!")
-
-    errors = calc_errors(Y, ground, problem)
-
-    # Save errors
-    with open(os.path.join(RES_DIR, "errors.json"), "w") as f:
-        json.dump(errors, f, indent=4)
-
-    print("Analyzing error indices.")
-    metric = problem['plot_settings']['metric']
-    np_errors = np.array([errors[out][metric] for out in problem['outputs']])
-
-    for i, out in enumerate(problem['outputs']):
-        si = asobol.analyze(problem, np_errors[i, :],
-                            print_to_console=False,
-                            calc_second_order=False,
-                            # parallel=True,
-                            # n_processors=MAX_WORKERS
-                            )
-        error_first_order_indices[i, :] = np.clip(si['S1'], 0, 1)
-        error_first_order_conf[i, :] = si['S1_conf']
-        error_total_order_indices[i, :] = np.clip(si['ST'], 0, 1)
-        error_total_order_conf[i, :] = si['ST_conf']
-        # second_order_indices[i, :] = np.clip(si['S2'], 0, 1)
-
-    np.save(f"{RES_DIR}/error_first_order_indices.npy",
-            error_first_order_indices)
-    np.save(f"{RES_DIR}/error_first_order_conf.npy",
-            error_first_order_conf)
-    np.save(f"{RES_DIR}/error_total_order_indices.npy",
-            error_total_order_indices)
-    np.save(f"{RES_DIR}/error_total_order_conf.npy",
-            error_total_order_conf)
-
-    print("Creating plots.")
-    plot(
-        first_order_indices,
-        total_order_indices,
-        Y,
-        errors,
-        param_values,
-        PLT_DIR,
-        T,
-        problem,
+    sa = SensitivityAnalysis(
+        model=model,
+        config=config
     )
 
-    print("Finished.")
+    sa.run(out_dir)
 
 
 if __name__ == "__main__":
