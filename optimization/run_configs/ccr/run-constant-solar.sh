0818b826bb0d11f1f6b10d3e2227dde64bb316b6
colinpannikkat <19521833+colinpannikkat@users.noreply.github.com>
2025-10-17 16:25:26 -0700

    Update run script

--- optimization/run_configs/ccr/run-constant-solar.sh
+++ optimization/run_configs/ccr/run-constant-solar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH -J garisom_optim_full-irrigation_ccr_leaftemp
+#SBATCH -J garisom_optim_constant-solar_ccr_leaftemp
 #SBATCH -c 32
 #SBATCH --mem=128G
 #SBATCH --time=2-00:00:00
@@ -10,10 +10,10 @@
 ml rclone gcc/14.3
 
 cd ~/hpc-share/fremont-cottonwood-dbg-garisom/
-git checkout full-irrigation
+git checkout constant-solar
 
 cd ./garisom/02_program_code
-git checkout full-irrigation
+git checkout constant-solar
 make clean
 make
 
@@ -23,13 +23,13 @@ source env/bin/activate
 cd ./optimization/
 
 EXP="leaftemp"
-EXP_DIR="output/ccr-full-irrigation"
+EXP_DIR="output/ccr-constant-solar"
 EXP_OUT="$EXP_DIR/${EXP}/optim.out"
 
 mkdir -p "${EXP_DIR}/${EXP}/"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - EXPERIMENT ${EXP} POPULATION CCR" >> $EXP_OUT
-python run_param_optim.py -i run_configs/ccr/${EXP}.json -o $EXP_DIR -m ../garisom/02_program_code -pd ../DBG-full-irrigation/ 2>&1 >> $EXP_OUT
+python run_param_optim.py -i run_configs/ccr/${EXP}.json -o $EXP_DIR -m ../garisom/02_program_code -pd ../DBG-constant-solar/ 2>&1 >> $EXP_OUT
 
 cd ..
 git checkout main
