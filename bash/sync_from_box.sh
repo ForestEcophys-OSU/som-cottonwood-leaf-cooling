955e058f000bb54d7cd411cc362bbcfff50db924
Colin <19521833+colinpannikkat@users.noreply.github.com>
2026-01-20 14:00:42 -0800

    Add leaf energy sa outputs to box sync scripts.

--- bash/sync_from_box.sh
+++ bash/sync_from_box.sh
@@ -1,4 +1,5 @@
 rclone copy --exclude-from='.gitignore' box:dbg-out/experiments ./sensitivity-analysis/experiments -v
 rclone copy --exclude-from='.gitignore' box:dbg-out/optimization ./optimization/output -v
 rclone copy --exclude-from='.gitignore' box:dbg-out/montecarlo ./montecarlo/output -v
+rclone copy --exclude-from='.gitignore' box:dbg-out/sa_leaf-energy ./leaf-energy-balance/sa/experiments -v
 # rclone check --exclude-from='.gitignore' ./experiments box:dbg-out/
--- bash/sync_to_box.sh
+++ bash/sync_to_box.sh
@@ -1,4 +1,5 @@
 rclone copy --exclude-from='.gitignore' ./sensitivity-analysis/experiments box:dbg-out/experiments -v
 rclone copy --exclude-from='.gitignore' ./optimization/output box:dbg-out/optimization -v
 rclone copy --exclude-from='.gitignore' ./montecarlo/output box:dbg-out/montecarlo -v
+rclone copy --exclude-from='.gitignore' ./leaf-energy-balance/sa/experiments box:dbg-out/sa_leaf-energy -v
 # rclone check --exclude-from='.gitignore' ./experiments box:dbg-out/
